c27cd2e8662a8f92b2549a6dac3531721cc616ea
yesseruser <yesseruseryt@gmail.com>
2023-12-08 21:58:04 +0100

    Added code.

--- extension.js
+++ extension.js
@@ -12,16 +12,19 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "firstext" is now active!');
+	console.log('Hello World Inserter running.');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('firstext.helloWorld', function () {
+	let disposable = vscode.commands.registerTextEditorCommand('firstext.insertHelloWorld', function (editor, edit) {
 		// The code you place here will be executed every time your command is executed
+		editor.selections.forEach((selection) => {
+			edit.insert(selection.active, '"Hello, world!"')
+		})
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FirstExt!');
+		vscode.window.showInformationMessage('Successfully inserted!');
 	});
 
 	context.subscriptions.push(disposable);
